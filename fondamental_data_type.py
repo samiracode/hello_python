64e96d8f6f138890f732c5cbfad69662fe481265
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-09-27 17:32:26 +0200

    int & float data type

---
+++ fondamental_data_type.py
@@ -0,0 +1,21 @@
+### Fundamental Data types
+
+## Int & float
+# float takes larger space in memory than an integer
+type(2+4) #int
+type(2-4) #int
+type(2/4) #float
+type(9.8 + 0.2) #float
+
+2 * 3 
+2 ** 3 # exponential (2 power of 3 )
+5 / 4  # rounded down to an integer so equals to 1
+5 % 4  # modular gives the remain of a division so equals to 1
+
+
+## Math Functions
+round(3.9) 
+abs(-20)  # returns the absolute value of an argument (absolute value = no negative ) so the answer is 20
+
+
+
