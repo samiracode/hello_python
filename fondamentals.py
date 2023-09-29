58079b9481efa198cfb0b644d707e77459f5c0b9
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-09-29 16:27:43 +0200

    conditionals & ternary operators

---
+++ fondamentals.py
@@ -0,0 +1,72 @@
+## Conditional logics
+
+is_old = True
+is_licenced = False
+
+#if is_old : 
+#   print('you are old enough to drive this car')
+#elif is_licenced :
+#    print('you are good to drive')
+#else : 
+#   print('Sorry! you can not drive this car')
+
+if is_old and is_licenced :
+    print('Have a good drive!')
+else :
+    print('Sorry! You can not drive this car')
+
+# Truthy & Falsy values 
+username = 'samira'
+password = '123'
+if username and password : # we are checking if thoese values exist; they are truthy
+    print('Welcome to your page')
+else : 
+    print('Please sign up for an account')
+# we can check truthy & falsy values on documentations
+
+## Ternary Operators
+# condition_if_true if condition else condition_else
+is_friend = False
+can_message = 'message allowed' if is_friend else 'not allowed to message'
+print(can_message)
+
+## Short Circuiting (or)(and)
+# (or) if one condition is correct, py does not check the second one
+# (and) if one condition is false, py does not check the other condition
+is_close = True
+is_economy = True
+if is_close or is_economy : 
+    print('good! Let\'s go there')
+else :
+    print('I just don\'t feel like to join you')
+
+## Logical operators
+# and, or, not, <, > , ==, !=, >=, <=, 
+print('a' > 'b')
+print('a' > 'A')
+print(not (False)) # not is also a function
+
+is_magition = True
+is_expert = False
+if is_magition and is_expert :
+    print('You are a master magition')
+elif is_magition and not is_expert :
+    print('At least you are getting there')
+elif not is_magition :
+    print('You need some magic!')
+
+## is vs == 
+print(True == 1) # True
+print('1' == 1)  # False
+print([] == 1)   # False
+print(10 == 10.0) # True
+print([] == [])   # True
+print([1,2,3] == [1,2,3]) # True
+
+print(True is 1) # if we make all the == above to 'is', they all ended up false
+# because == compares the values of but 'is' is pointing at a place in memory 
+# Where all these values are alocated, as they can not be the same, it prints False
+print ([] is []) # False
+# print(value is value) True
+# print( data_structure is data_structur) False
+
