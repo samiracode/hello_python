0445929c40c024d2104e9b799421ba82c0630c82
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-09-30 12:45:31 +0200

    *args & **kwargs 2

--- functions.py
+++ functions.py
@@ -37,11 +37,11 @@ def say_hello1(name="Arthur", emoji='xD ') :
 
 ## Return 
 # a function weather modifys something or returns something
-def sum (num1, num2) :
+def add_nums (num1, num2) :
     def another_fun (n1, n2) :
         return n1 + n2
     return another_fun(num1, num2)
-total = sum(10,20)
+total = add_nums(10,20)
 print(total)
 
 ## tesla exersize :
@@ -70,6 +70,47 @@ help(test)
 # or magic method or dunder method
 print(test.__doc__)
 
+## Arguments & keyword arguments
+ # *args & **kwargs
 
+
+def super_func(*args):
+    return sum(args)
+
+print(super_func(1, 2, 3, 4, 5))
+
+def duper_func(*args, **kwargs) :
+    total = 0
+    for item in kwargs.values() :
+        total += item
+    return sum(args) + total
+
+print(duper_func(1,2,3,4,5, n1=6, n2=7))
+
+# Rule for using parameters in function
+# First parmaeters
+# Second **args
+# Third default parameters
+# Fourth **kwargs
+
+## Ex:
+def highest_even (li) :
+    evens = []
+    for n in li:
+        remain = n % 2
+        if remain == 0 :
+            evens.append(n)
+    if evens == []:
+        print('There is no even numbers in the list')
+    else :
+        print(max(evens))
+
+     
+
+            
+highest_even([1,3,5,7,8,4,6,12])
+            
+
+           
 
 
