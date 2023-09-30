4c52a394fcea872b0951132c9282d6a3ca087678
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-09-30 09:28:18 +0200

    parameters & arguments

--- functions.py
+++ functions.py
@@ -18,3 +18,21 @@ def show_tree():
 
 show_tree()
 print(show_tree)
+
+# Positional Parameters (they are called positional because their position matters)
+def say_hello (name, emoji) :
+    print(f'Hello {name} {emoji}')
+
+# Positional Arguments
+say_hello('Charlie', ':)')
+say_hello('Leonard', ':D')
+say_hello('Sally', ':p')
+
+# Keyword arguments (position does not matter) => bad practice !!!
+say_hello(emoji=':)', name='Andrei')
+
+# default parameters (incase we call the function and we do not give any argument, the default argument will be run)
+def say_hello1(name="Arthur", emoji='xD ') :
+  print(f'Hello {name} {emoji}')
+
+
