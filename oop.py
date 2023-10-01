e1216eb9ffbafdf7b93262d3a2b71dba79e0cee7
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-01 11:38:50 +0200

    attributes & methods with amazing Ferrari

--- oop.py
+++ oop.py
@@ -16,5 +16,33 @@ player3 = PlayerCharacter('Charlie', 2)
 
 print(player1.name)
 
+## Methods & Attrbiutes
+class RacingCars :
+    def __init__(self, name, model, color, max_speed) :
+        self.name = name # attribute
+        self.model = model
+        self.color = color
+        self.max_speed = max_speed
+    def intro(self) :
+        return print(f'The {self.name} is an amazing {self.model} car.And the {self.color} is a classic.')
+    
+    def race(self) :
+        return print(f'{self.name} is speeding to {self.max_speed}')
+    
+car1 = RacingCars('Ferrari',2006, 'red', '340 km/h')
+car2 = RacingCars('Aston Martin',2008, 'black', '354 km/h')
+car3 = RacingCars('Mercedes',2014, 'silver', '321 km/h')
+
+car1.intro()
+car2.intro()
+car3.intro()
+
+car1.race()
+car2.race()
+car3.race()
+
+
+        
+
 
     
