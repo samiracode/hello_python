d148739002184445cb5d4a339f21907841dd0e66
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-02 19:24:04 +0200

    enheritance

--- oop.py
+++ oop.py
@@ -1,6 +1,8 @@
 ### Object Orienting Programming ¨
 # what is an object?
 # object has methods and attributes that we can use with DOT method
+#What is the purpose of OPP? To creat our own data-type!
+
 class PlayerCharacter :
     def __init__(self, name, age) :
         self.name = name
@@ -66,6 +68,7 @@ def get_oldest_cat(*args):
 print(f"The oldest cat is {get_oldest_cat(peanut.age, garfield.age, snickers.age)} years old.")
 
 ## @classmethod & @staticmethod
+# are called decorators 
 
 class PlayerId :
     membership = True #attribute
@@ -77,13 +80,70 @@ class PlayerId :
     def adding_things(cls, badge1, badge2) :
         return cls('Tedy', badge1 + badge2 ) # with cls at this line we are instantiating an object
     
-    @staticmethod # Exact like @classmethod we just do not care any more about class state in this method
-    def adding_things( badge1, badge2) :
-        return  badge1 + badge2 
+    #@staticmethod # Exact like @classmethod we just do not care any more about class state in this method
+    #def adding_things( badge1, badge2) :
+    #    return  badge1 + badge2 
 
 player10 = PlayerId.adding_things(2,3)
 print(player10.score)
 
+## 4 PILLARS OF OOP
+
+# ENCAPSULATION is binding of data & functions that manipulate the data
+
+# ABSTRACTION means hiding of information or abstracting away information
+# and giving access to only what's neccesary
+   # Private vs Public variables
+
+# private ? we use _varible's name to show that is a private variable
+# but that has no power; it is just a convention to show to others not ot 
+# over ride this variable
+class Fruit :
+    def __init__(self,name, color):
+        self._name = name
+        self._color = color
+    
+    def run (self):
+       print('run')
+    
+    def speak(self):
+        print(f'Hi I am {self.name} and my color is {self.color}')
+    
+fruit1 = Fruit('apple', 'red')
+fruit2 = Fruit('banana', 'yellow')
+fruit3 = Fruit('pearl', 'green')
+
+fruit1._name = '!!!!'
+fruit1.speak = 'BOOOO'
+
+# ENHERITANCE allows new objects to take on the properties of exicting object
+
+class User :  # parent class
+    def sign_in(self): # No __init__ method; we could have it but if we do not have any variable or attributes
+        print('logged in!') # that we want to assign to user, then we would not need any __init__ method
+
+class Wizard(User) :  # children classes = sub-classes = drived-classes
+    def __init__(self, name, power):
+        self.name = name
+        self.power = power
+    
+    def attack(self):
+        print(f'atacking with the power of {self.power}.')
+        
+
+class Archer(User) : # children classes = sub-classes = drived-classes
+    def __init__(self, name, num_arrows):
+      self.name = name
+      self.num_arrows = num_arrows
+    
+    def attack(self):
+        print(f'atacking with the arrows. Number of arrows left : {self.num_arrows}')
+
+wizard1 = Wizard('Marlin', 50)
+archer1 = Archer('Robin', 100)
+wizard1.attack()
+archer1.attack()
+    
         
 
 
