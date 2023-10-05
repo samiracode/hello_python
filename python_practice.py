943779560796222cb322681953aa687b90edc080
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-05 15:38:37 +0200

    oldest cat practice

--- python_practice.py
+++ python_practice.py
@@ -1,18 +1,34 @@
-class Student:
+
+
+
+class Cat:
+    species = 'mammal'
     def __init__(self, name, age):
         self.name = name
         self.age = age
+    
+    def oldest_cat(self):
+        print(self.age)
 
-    def intro(self):
-        print(f'Hi {self.name}! You are {self.age} years old')
 
-student1 = Student('Johnny', 11)
-student2 = Student('Sally', 8)
-print(student1.name)
-print(student2.name)
 
-print(Student.intro(student1))
-print(Student.intro(student2))
+# 1 Instantiate the Cat object with 3 cats
+cat1 = Cat('Johnny',8)
+cat2 = Cat('Sally', 6)
+cat3 = Cat('Andre', 9)
+
+print(cat1.oldest_cat())
+
+
+
+# 2 Create a function that finds the oldest cat
+
+
+  
+
+
+
+# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
 
 
 
