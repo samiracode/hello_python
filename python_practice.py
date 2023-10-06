d13a763d1906a12226ba789a66eb46792ccf672d
Samira Tavakolli <104760134+samiracode@users.noreply.github.com>
2023-10-06 10:40:19 +0200

    encapsulation ex.

--- python_practice.py
+++ python_practice.py
@@ -1,35 +1,12 @@
-
-
-
-class Cat:
-    species = 'mammal'
+class Player:
     def __init__(self, name, age):
         self.name = name
         self.age = age
     
-    def oldest_cat(self):
-        print(self.age)
-
-
-
-# 1 Instantiate the Cat object with 3 cats
-cat1 = Cat('Johnny',8)
-cat2 = Cat('Sally', 6)
-cat3 = Cat('Andre', 9)
-
-print(cat1.oldest_cat())
-
-
-
-# 2 Create a function that finds the oldest cat
-
-
+    def shout(self):
+        print(f'{self.name} is shouting')
+    
+player1 = Player('Samira', 41)
+player1.shout()
   
-
-
-
-# 3 Print out: "The oldest cat is x years old.". x will be the oldest cat age by using the function in #2
-
-
-
 
